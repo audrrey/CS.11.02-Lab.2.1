e9ba8243d8ef90c30d5b5f777ab26ec550f88f0f
Audrey Tan <audreyt2026@student.cis.edu.hk>
2023-09-27 12:47:18 +0800

    lab 2 done

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,52 @@ public class Main {
     }
 
     // 1. add
-    
+    public static int add(int a, int b)
+    {
+        return a+b;
+    }
+
     // 2. add
+    public static int add(int a, int b, int c, int d)
+    {
+        int ab = add(a, b);
+        int cd = add(c, d);
+        return add(ab, cd);
+    }
 
     // 3. morningGreeting
+    public static String morningGreeting (String personsName)
+    {
+        return "早上好, " + personsName + "!";
+    }
 
     // 4. afternoonGreeting
+    public static String afternoonGreeting (String personsName)
+    {
+        return "下午好, " + personsName + "!";
+    }
 
     // 5. triple
+    public static String triple (String text)
+    {
+        return text + text + text;
+    }
 
     // 6. half
+    public static double half (int number)
+    {
+        return number/2.0;
+    }
 
     // 7. roundPositiveValueToNearestInteger
-
+    public static int roundPositiveValueToNearestInteger (double n)
+    {
+        return (int) Math.round(n);
+    }
     // 8. roundNegativeValueToNearestInteger
+    public static int roundNegativeValueToNearestInteger  (double n)
+    {
+        return (int) -Math.round(-n);
+    }
 
 }
